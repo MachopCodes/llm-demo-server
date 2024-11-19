5fdbd9aa64eb4bb1c5b83c9d049e47bd9882b7af
Will Andreae <wcandreae@gmail.com>
2024-11-18 19:22:28 -0500

    silly error

--- api/app.py
+++ api/app.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 CORS(app, origins=[
     "http://localhost:3000", 
     "http://localhost:3001", 
-    "https://https://machopcodes.github.io"
+    "https://machopcodes.github.io"
     ])
 
 
